6862ccccac33a26670c01096a001ae7c369e27c0
1018807775 <114075342+1018807775@users.noreply.github.com>
2022-09-21 20:57:07 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,20 +1,29 @@
 {
-#公众号配置
-#公众号appid
+# 公众号配置
+# 公众号appId
 "app_id": "wxe49647bf551b2960",
-#公众号appSecret
-"app_secret":"4429be3aab4aabcd464650cd19f6ddc0",
-#模板消息id
-"template_id":"GZsGndLqQtrwY_KS0CLAqsRXBrX1YBWOC_ZpGU8tmzE",
-#接收公众号消息的微信号，如果有多个，需要在[]里用英文逐号间隔，例如["wx1","wx2"]
-"user":["oZeIE6X_ZYr0M5nF9YGMaCY5P5lI"],
-#信息配置
-#所在省份
-"province":"广西",
-# 所在城市
-"city": "桂林",
-#生日1，修改name为对应需要显示的名字，如果生日为农历，在最前面加上r即可
-"birthday1":[name":"伍纤纤”"birthday":"2002-11-18”],
-#在一起的日子，格式同上
-"love date":["name":"在一起的日子”"anniversary":"2021-11-18”],
+# 公众号appSecret
+"app_secret": "22626d5f6a011dda9af8803add738c98",
+# 模板消息id
+"template_id": "jbJZInhrcELERp2QycP846lDLJsFbtj6NODrgLbkjpM",
+# 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["weixin1", "weixin2"]
+"user": ["oZeIE6doEB0scpDbwkKKEf0WKo8c"],
+ 
+# 信息配置
+# 和风天气apikey
+"weather_key": "c72386dff14e430b8332f5bd8b5243f4",
+# 所在地区，可为省，城市，区，县，同时支持国外城市，例如伦敦
+"region": "桂林市",
+# 生日1
+# 修改名字为对应需要显示的名字
+# 如果生日为农历，在最前面加上 r 即可，不加 r 默认为阳历
+"birthday1": {"name": "伍纤纤", "birthday": "2002-11-18"},
+# 生日2
+"birthday2": {"name": "袁淞栩", "birthday": "2002-04-28"},
+# 在一起的日子，格式同上，暂不不支持农历
+"love_date": "2021-11-18",
+# 金句中文，如果设置了，则会显示这里的，如果为空，默认会读取金山的每日金句
+"note_ch": "",
+# 金句英文
+"note_en": ""
 }
